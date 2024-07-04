533dd3fee4668707c78f01ef023ee071f213f459
ma-maimo <monmamai0916@gmai.com>
2024-07-04 20:51:04 +0900

    初期データの登録の再提出

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // DB::table('users')->insert([
+        //     "over_name" => "ちい",
+        //     "under_name" => "かわ",
+        //     "over_name_kana" => "チイ",
+        //     "under_name_kana" => "カワ",
+        //     "mail_address" => "chiikawa@chiikawa.com",
+        //     "sex" => "2",
+        //     "birth_day" => "1995-05-01",
+        //     "role" => "4",
+        //     "password" => bcrypt('chiikawa1234') //bcrypt( )→暗号化
+        // ]);
 
+        $this->call([
+            UsersTableSeeder::class,
+            SubjectsTableSeeder::class,
+        ]);
     }
 }
